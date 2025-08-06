594f99fc7fbc0e9acb8bb209abb949dc323e150e
aleu0007 <aleu0007@student.monash.edu>
2025-08-06 12:11:56 +1000

    score card + fancy text prefab
    score card + fancy text prefab

--- Assets/Scripts/FancyTextController.cs
+++ Assets/Scripts/FancyTextController.cs
@@ -12,22 +12,47 @@ using TMPro;
 public class FancyTextController : MonoBehaviour
 {   
     [SerializeField] private TextMeshProUGUI[] textFields;
-    [TextArea]
     [SerializeField] private string editorText = "Default Text";
 
-    [ContextMenu("Update Texts")]
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnValidate()
+    {
+        // This runs in the editor when values change
+        UpdateTextFields();
+    }
+
     public void Awake()
     {
-         if (textFields == null || textFields.Length == 0)
+        // Automatically find text fields if not assigned
+        if (textFields == null || textFields.Length == 0)
         {
             textFields = GetComponentsInChildren<TextMeshProUGUI>();
         }
+        
+        // Update text fields at runtime
+        UpdateTextFields();
     }
 
-    // Update is called once per frame
+    [ContextMenu("Update Texts")]
     public void SetEditorText()
     {
+        UpdateTextFields();
+    }
+
+    public void SetText(string newText)
+    {
+        editorText = newText;
+        UpdateTextFields();
+    }
+
+    private void UpdateTextFields()
+    {
+        // Ensure we have text fields
+        if (textFields == null || textFields.Length == 0)
+        {
+            textFields = GetComponentsInChildren<TextMeshProUGUI>();
+        }
+
+        // Update all text fields
         foreach (var tmp in textFields)
         {
             if (tmp != null)
