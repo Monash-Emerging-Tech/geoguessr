04920d3e9386f47162866d245693b0a39de71bb5
O-Bolt <p3bbl3bo1@gmail.com>
2025-08-11 01:05:57 +1000

    Add documentation to GameLoader

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 using static LocationManager;
 
 
-
+/***
+ * 
+ * GameLoader, Responsible for the in game events and keeping track of players score. 
+ * 
+ * Written by O-Bolt
+ * Last Modified: 08/08/2025
+ * 
+ */
 public class GameLoader : MonoBehaviour
 {
 
+    // Public Variables //
     public bool inGame;
     public int currentScore;
     public int currentRound;
@@ -16,7 +24,7 @@ public class GameLoader : MonoBehaviour
     public int GameMode;
     public int mapPackId = 0;
 
-
+    // Used to find the LocationManager variables
     public LocationManager locationManager = new LocationManager();
 
     private List<LocationManager.MapPack> mapPacks;
@@ -24,29 +32,56 @@ public class GameLoader : MonoBehaviour
 
     public static GameLoader Instance; // Global reference
 
+
+    // Moves to the Game Scene for the game to start, performs all thes start game logic
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
         inGame = true;
+        currentRound = 0;
+        currentScore = 0;
         List<LocationManager.MapPack> mapPacks = locationManager.GetMapPacks(); 
     }
 
+
+    // Not too sure how this works but this is called after the Awake() function for MonoBehaviours,
+    // This calls the OnSceneLoaded Function, Not sure how it really works or anything
+    // https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager-sceneLoaded.html 
     void OnEnable()
     {
         Debug.Log("OnEnable called");
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    // Function is called when the scene is loaded
+    // https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager-sceneLoaded.html
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        // If the scenes is the GameScene, we should now change the Map and let the game start
         if (scene.name == "GameScene") {
-            locationManager.setCurrentMapPack(mapPackId);
-            locationManager.SelectRandomLocation();
+            changeMap();
         }
         
         Debug.Log("Scene name: " + scene.name);
     }
 
+
+    public void nextRound() {
+        currentRound++;
+        // currentScore += ; // Update the score eventually
+        changeMap();
+
+    }
+
+
+    // Changes the map shown to the player at the start of a new round
+    public void changeMap() {
+        locationManager.setCurrentMapPack(mapPackId);
+        locationManager.SelectRandomLocation();
+    }
+
+
+    // This ensures that there is only one GameLoader in the game at one time, and ensures that the GameObject is not destroyed
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
