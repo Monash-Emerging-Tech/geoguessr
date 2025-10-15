7ef3eca98943f8c0a8b4f650f6a33309530dae79
madyyson <madisonckss@gmail.com>
2025-10-15 15:36:33 +1100

    Changed scorebar masking such that it does not require a manual input from user in unity but instead depend on 'current' variable.
    Changed scorebar masking such that it does not require a manual input from user in unity but instead depend on 'current' variable.

--- Assets/Scripts/ScoreBar.cs
+++ Assets/Scripts/ScoreBar.cs
@@ -6,32 +6,24 @@ using UnityEngine.UI;
  * ScoreBar, Responsible for displaying the score bar with a progress bar, 
  * 
  * Written by aleu0007
- * Last Modified: 1/08/2025
+ * Last Modified: 31/01/2026
  * 
  */
 [ExecuteInEditMode()]
 public class ScoreBar : MonoBehaviour
-{   
-    public int maximum;
-    
-    public Image mask;
-    // [SerializeField] private string editorText = "Default Text";
-    [SerializeField] private ScoreDataScriptableObject scoreData;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+{
+    private int maximum = 2500;
+    private int current = 1000;
+    [SerializeField] private Image mask;
 
     void Awake() => Refresh();
     void OnValidate() => Refresh();             // so it updates in the Editor
 
     private void Refresh()
     {
-        int current = (scoreData != null)? scoreData.CurrentScore: 0;
-        float fillAmount = (float)current/(float)maximum;
+        if (!mask) return;
+
+        float fillAmount = (float)current / maximum;
         mask.fillAmount = fillAmount;
-    }    
+    }
 }
