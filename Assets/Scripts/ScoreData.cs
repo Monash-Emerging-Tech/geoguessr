3faa4bb951b9c2342691ee704b37f6f0e3b2753c
O-Bolt <p3bbl3bo1@gmail.com>
2026-02-09 16:22:11 +1100

    Update Score Calculation
    Score Calculate is handled by Score Data and uses the new guide

--- Assets/Scripts/ScoreData.cs
+++ Assets/Scripts/ScoreData.cs
@@ -40,4 +40,48 @@ public class ScoreDataScriptableObject : ScriptableObject
 
     }
 
+
+
+    public static int CalculateScore(int distance)
+    {
+
+
+        switch (distance)
+        {
+            case <= 3:
+                return 500;
+
+            case <= 25:
+                return CalculateInterval(distance, 4, 25, 495, 450);
+
+            case <= 50:
+                return CalculateInterval(distance, 26, 50, 447, 350); 
+
+            case <= 250:
+                return CalculateInterval(distance, 51, 250, 345, 200); 
+
+            case <= 300:
+                return CalculateInterval(distance, 251, 300, 198, 100); 
+
+            case <= 500:
+                return CalculateInterval(distance, 301, 500, 98, 10); 
+
+            case <= 700:
+                return CalculateInterval(distance, 501, 700, 10, 0); 
+
+            default:
+                return 0;
+        }
+
+        int CalculateInterval(int distance, int minDis, int maxDis, int maxScore, int minScore) {
+
+            double gradient = (double)(minScore - maxScore) / (maxDis - minDis); // Negative gradient
+
+            int score = (int)Math.Round(maxScore + gradient*(distance - minDis));
+
+            return score;
+        }
+
+    }
+
 }
